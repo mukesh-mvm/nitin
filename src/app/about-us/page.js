aae1b672b2e58536a1259f29dbc3424bffb1e890
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2026-06-02 11:49:39 +0530

    new commit

--- src/app/about-us/page.js
+++ src/app/about-us/page.js
@@ -1,11 +1,13 @@
-import AboutUs from "@/componets/about/AboutUs ";
-import HomeMain from "@/componets/home/HomeMain";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    <AboutUs/>
+    {/* <AboutUs/> */}
+    <div>
+      about const first 
+    </div>
     </>
   );
 }
